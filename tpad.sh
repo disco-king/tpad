8f74a52c6d2eb40cfa0aab53ce224330ebd9f213
disco-king <acalabash@gmail.com>
2022-02-14 13:41:48 +0300

    some compatibility modifications

--- tpad.sh
+++ tpad.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/bash
 
-id=$(xinput list | grep 'TouchPad' | \
-	awk '{print $6}' | cut -d '=' -f2)
+id=$(xinput list | awk '/[Tt]ouch[\-]?[Pp]ad/{print $6}' | cut -d '=' -f2)
 
-state=$(xinput list-props $id | awk '/ *Device Enabled/{print $4}')
+if [ -z $id ]
+then
+	echo "Xinput entry not found"
+	echo "Check device name"
+	exit 1
+fi
+
+state=$(xinput list-props $id | awk '/Device Enabled/{print $4}')
 
 if [ $state -eq 1 ]
 then
-	echo "Disabling TouchPad"
+	echo "Touchpad disabled"
 else
-	echo "Enabling TouchPad"
+	echo "Touchpad enabled"
 fi
 
 xinput set-prop $id 'Device Enabled' $((($state + 1) % 2))
