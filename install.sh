33f84fb3b2805aef103c19a77df6b3de31ba2b1c
disco-king <acalabash@gmail.com>
2022-03-16 15:12:20 +0300

    added install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+if [ -z $1 ]
+then
+	echo "Provide an option for installation"
+	echo "Use \"help\" or -h to get help"
+	exit 1
+fi
+
+if { [ $1 == "-h" ] || [ $1 == "help" ]; } 
+then
+	echo "This script creates \"tpad\" alias in ~/.bash_aliases"
+	echo "run \"install.sh here\" to save this location of tpad.sh"
+	echo "or \"install.sh [path]\" to provide a custom path"
+	echo "where tpad.sh will be copied automatically"
+	exit 0
+fi
+
+if [ $1 == "here" ] 
+then
+	path=$(pwd)"/tpad.sh"
+else
+	path=$1"/tpad.sh"
+	cp tpad.sh $path
+fi
+
+echo "Adding alias \"tpad\" for $path"
+echo
+
+if ! echo "alias tpad=$path" >> ~/.bash_aliases
+then
+	echo "Alias creation failed"
+	exit 1
+fi
+
+echo "Alias creation successfull"
